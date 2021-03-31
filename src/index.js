2fb39805322ebb29a70d7737a490ebcf5b85b992
biktop <mantorovv@gmail.com>
2021-03-31 08:49:18 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+import './scss/index.scss';
+
+
+document.querySelector('.user-desc__button').addEventListener('click', function (e) {
+  this.classList.toggle('active');
+});
+
+console.log('Hello World!');
